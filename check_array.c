5262dc12ac5068338ed890a55c5f144033d9cc8b
Jabulani-N <jabu.94@gmail.com>
2023-04-20 16:40:14 -0700

    updated_array_checker_for_extra_funcitonality

--- check_array.c
+++ check_array.c
@@ -1,7 +1,8 @@
 /**
  * check-array - checks if the entire array is in order
  * @array: the array in question
- * Return: 1 if it is in order. 0 otherwise.
+ * Return: -1 if it is in order.
+ * index of flaw [x] otherwise.
  */
 
 int check-array(int array[])
@@ -11,7 +12,7 @@ int check-array(int array[])
 	for(x = 0; array[x+1]; x++)
 	{ /*as long as there is a next to compare to*/
 		if (array[x] > array[x+1])/*there are items out of order*/
-			return(0);/*fail one fails all. return immediately*/
+			return(x);/*fail one fails all. return immediately*/
 	}
-	return (1);/*got through with no violations*/
+	return (-1);/*got through with no violations*/
 }
