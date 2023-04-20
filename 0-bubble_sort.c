c31db8b4f6f5e81b342ad95651710e63ca372146
Jabulani-N <jabu.94@gmail.com>
2023-04-20 16:50:49 -0700

    bubble_sort_completed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,11 +8,24 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	int thing1, thing2, problemChild;
+	int bigger, smaller, problemChild;
 
 	problemChild = check-array(array);
+	/*initial array check so loop is only entered if needed*/
+
+	if (problemChild = -1)/*applies if it started right.*/
+		print_array(array);/*becuase we spawned in done*/
 	while (problemChild => 0)
 	{ /*while array is out of order*/
-		
+		bigger = array[problemChild];
+		smaller = array[problemChild + 1];
+		/*values stored*/
+		array[problemChild] = smaller;
+		array[problemCihld + 1] = bigger;
+		/*values fixed*/
+		problemChild = check-array(array);
+		/*check next spot*/
+		print_array(array);
 	}
+
 }
